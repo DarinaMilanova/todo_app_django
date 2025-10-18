6a11aec3ba39418f7a6c93b59e7029b41ffce4d6
Darina <darinakmilanova@gmail.com>
2025-10-18 23:04:47 +0300

    Update signals.py

--- tasks/signals.py
+++ tasks/signals.py
@@ -1,11 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Category   # ✅ relative import
+from .models import Category   # relative import
 
 @receiver(post_save, sender=User)
 def create_default_categories(sender, instance, created, **kwargs):
-    if created:  # ✅ only when new user is created
+    if created:  # # only when new user is created
         default_categories = [
             "Work",
             "Personal",
